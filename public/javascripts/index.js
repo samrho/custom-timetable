2813bac2aa1f70d962d79491f79d4e65fd825efb
Sam Rho <seungnam2@gmail.com>
2019-11-02 15:04:34 +0900

    api: add an api for registering the lecture
    localhost:3000/api/add/PG1807-18이 POST request를 통해 들어오면 PG1807-18에 해당하는 강의를 찾아서 충돌이 없다면 등록하게 하는 api 뼈대 완성

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,23 @@
-$(".card-lecture").click(function() {
+import { getCodeName } from "./utils.js";
+
+$(".card-lecture").click(function(event) {
 	$("#modal-lecture-info").modal("show");
+	const data = event.currentTarget;
+	const modal = $("#modal-lecture-info");
+	modal.find(".lecture-title").text(data.childNodes[0].innerText);
+	modal
+		.find(".lecture-time > span")
+		.text(data.childNodes[1].childNodes[1].innerText);
+	modal
+		.find(".lecture-code__code > span")
+		.text(data.childNodes[2].childNodes[0].innerText);
+	modal
+		.find(".lecture-code__prof > span")
+		.text(data.childNodes[2].childNodes[1].innerText);
+
+	modal
+		.find(".lecture-code__location > span")
+		.text(data.childNodes[2].childNodes[2].innerText);
 });
 
 $(".lecture-time > a").click(function() {
@@ -21,3 +39,23 @@ $(function() {
 		},
 	});
 });
+
+const lecture = document.querySelector(".registerBtn");
+lecture.addEventListener("click", (e) => {
+	const selectedLecture =
+		e.target.parentNode.previousSibling.childNodes[1].childNodes[1];
+	const selectedCodeName = getCodeName(selectedLecture);
+	console.log(selectedCodeName);
+	registerOnTheTable(selectedCodeName);
+});
+console.log(lecture.parentNode);
+
+const registerOnTheTable = (lectureCode) => {
+	const URL = `/api/add/${lectureCode}`;
+	fetch(URL, {
+		method: "POST",
+	})
+		.then((res) => res.json())
+		.then((res) => console.log(res));
+	$("#modal-lecture-info").modal("hide");
+};
