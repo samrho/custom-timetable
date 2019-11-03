a6e0744df49959024f5d12a0cfeed96ad9794ed7
Sam Rho <seungnam2@gmail.com>
2019-11-03 14:30:29 +0900

    refactor: reduce redundancy from separating the logic
    하드코딩된 시간을 반복문을 이용하여 조금 더 짧은 길이로 생성해 줄 수있게 모듈화를 하였다.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const lectures = require("../models").Lectures;
+const { getTimesArray } = require("../util/utils");
 
 router.get("/", async function(req, res, next) {
 	const data = await lectures.findAll({ raw: true });
-	console.log(data);
-	res.render("index", { lectures: data });
+	const times = getTimesArray();
+	res.render("index", { lectures: data, times });
 });
 
 module.exports = router;
