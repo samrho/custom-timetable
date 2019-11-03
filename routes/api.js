34b3f313d56aa2027b4a67d71db7c379742dbf86
Sam Rho <seungnam2@gmail.com>
2019-11-03 21:56:53 +0900

    middleware: set middlewares for the apis

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const {
 	collisionCheck,
 	successfullyAdded,
+	findLectureByName,
+	successfullyRemoved,
 } = require("../middlewares/register.middle");
 
 router.post("/add/:code", collisionCheck, successfullyAdded);
-
+router.get("/find/:lecture", findLectureByName);
+router.post("/delete/:code", successfullyRemoved);
 module.exports = router;
