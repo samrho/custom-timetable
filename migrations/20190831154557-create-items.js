b610ec209d8a94cc0425ba538a22161a53084b74
Sam Rho <seungnam2@gmail.com>
2019-11-03 21:54:27 +0900

    db: add a  column to the table

--- migrations/20190831154557-create-items.js
+++ migrations/20190831154557-create-items.js
@@ -33,6 +33,10 @@ module.exports = {
 				type: Sequelize.BOOLEAN,
 				defaultValue: false,
 			},
+			description: {
+				type: Sequelize.STRING,
+				defaultValue: `본 강의에서는 ABC를 이용한 EFG 기반 프로그래밍 기초 및 응용기술에 대해 학습합니다. 특히 실습 위주의 수업으로 프로그래밍 스킬 향상 및 실무 능력을 갖출 수 있도록 합니다.`,
+			},
 			memo: {
 				type: Sequelize.STRING,
 			},
