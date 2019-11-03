ff43d7b82b801241e97d90d3d91f5f22450dd4f0
Sam Rho <seungnam2@gmail.com>
2019-11-03 18:22:46 +0900

    fix: change filter algorithm to capitalize letters

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -20,9 +20,39 @@ $(".card-lecture").click(function(event) {
 		.text(data.childNodes[2].childNodes[2].innerText);
 });
 
-$(".lecture-time > a").click(function() {
+$(".lecture-time > a").click(async function(event) {
 	$("#modal-lecture-task").modal("show");
+	const data = event.currentTarget;
+	const selectedLectureName = data.childNodes[0].childNodes[0].textContent;
+	const URL = `/api/find/${selectedLectureName}`;
+	const tempResult = await fetch(URL);
+	const result = await tempResult.json();
+	const modal = $("#modal-lecture-task");
+	const timeAndDaysString = `${result.data.start_time}:00 - ${result.data.end_time}:00 | ${result.data.dayofweek}`;
+	const memo = result.data.memo || "memo를 입력해 주세요";
+	modal.find(".lecture-time > span").text(timeAndDaysString);
+	modal.find(".modal-body .lecture-title").text(selectedLectureName);
+	modal.find(".lecture-code__code > span").text(result.data.code);
+	modal.find(".lecture-code__prof > span").text(result.data.professor);
+	modal.find(".lecture-code__location > span").text(result.data.location);
+
+	modal.find(".txt-description").text(result.data.description);
+	modal.find(".lecture-noti-title").text(memo);
+	modal.find(".modal-footer .right .btn-danger").on("click", () => {
+		removeFromTable(result.data.code);
+		modal.modal("hide");
+		location.reload();
+	});
 });
+
+async function removeFromTable(lectureCode) {
+	const URL = `/api/delete/${lectureCode}`;
+	const result = await fetch(URL, {
+		method: "POST",
+	});
+	const { message } = await result.json();
+	// if (message) alert(message);
+}
 
 $(function() {
 	$('[data-toggle="tooltip"]').tooltip();
@@ -40,6 +70,13 @@ $(function() {
 	});
 });
 
+const memoAddBtn = document.querySelector(".btn-save");
+memoAddBtn.addEventListener("click", (event) => {
+	// const memoTitle = $("#recipient-name").value;
+	// const memoContent = $("#message-text").value;
+	alert("hi");
+});
+
 const lecture = document.querySelector(".registerBtn");
 const registerHandler = (e) => {
 	const selectedLecture =
@@ -55,25 +92,21 @@ const registerOnTheTable = async (lectureCode) => {
 	const result = await fetch(URL, {
 		method: "POST",
 	});
-	const { message } = await result.json();
-	if (message) alert(message);
-	// if (result.ok) {
-	// 	document.querySelector("body").innerHTML = `<h1>${result}</h1>`;
-	// }
-
+	const { message, OK } = await result.json();
+	if (!OK) alert(message);
 	$("#modal-lecture-info").modal("hide");
-	// location.reload();
+	location.reload();
 };
-
 //search control
 const input = document.querySelector(".form-control");
 const searchFilterHandler = (e) => {
 	const filter = e.target.value.toUpperCase();
+	console.log(filter);
 	const lectureList = document.querySelectorAll(".card-lecture");
 	lectureList.forEach((lecture) => {
 		const [title, code, prof] = getLectureInfo(lecture);
 		if (
-			title.includes(filter) ||
+			title.toUpperCase().includes(filter) ||
 			code.includes(filter) ||
 			prof.includes(filter)
 		) {
