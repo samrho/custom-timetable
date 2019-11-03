48494fceed8e563857211dc254f8d9adce9da399
Sam Rho <seungnam2@gmail.com>
2019-11-03 14:32:17 +0900

    api: modify  api to use two middlewares
    DB 데이터 충돌을 감지하는 collisionCheck 미들웨어와 성공적인 업데이트를 처리하는 successfullyAdded 미들웨어

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const lectures = require("../models").Lectures;
-const collisionCheck = require("../middlewares/collisionCheck");
-router.post("/:id", async function(req, res, next) {
-	res.status(200).json({
-		message: "done",
-	});
-});
-router.post("/add/:code", collisionCheck, async function(req, res, next) {
-	const { code } = req.params;
-	console.log(code);
-	res.status(200).json({
-		message: "done",
-	});
-});
+const {
+	collisionCheck,
+	successfullyAdded,
+} = require("../middlewares/register.middle");
+
+router.post("/add/:code", collisionCheck, successfullyAdded);
 
 module.exports = router;
